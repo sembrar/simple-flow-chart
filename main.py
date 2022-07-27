2146099532b0763cb8963153a37b1bfe988e0b23
sembrar <sagarpj95@gmail.com>
2022-07-27 14:02:56 +0530

    Asks to overwrite loaded commands text file if text contents are changed in the GUI
    On branch main
    Changes to be committed:
     modified:   main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 
 import tkinter
 from tkinter import ttk
+from tkinter import messagebox
 
 from MyModules.Widgets.ScrolledWidgets.ScrolledCanvas import ScrolledCanvas
 from MyModules.Widgets.ScrolledWidgets.ScrolledText import ScrolledText
@@ -13,6 +14,8 @@ class FlowChart(tkinter.Tk):
     def __init__(self, commands_text_file_path=None):
         tkinter.Tk.__init__(self)
         self.title("FlowChart")
+
+        self._commands_text_file_path = commands_text_file_path
 
         paned_window = ttk.Panedwindow(self, orient=tkinter.HORIZONTAL)
         paned_window.grid(row=0, column=0, sticky='news')
@@ -36,12 +39,31 @@ class FlowChart(tkinter.Tk):
         label_frame_for_text.rowconfigure(0, weight=1)
         label_frame_for_text.columnconfigure(0, weight=1)
 
+        self._original_text = None
         if commands_text_file_path is not None:
             try:
                 with open(commands_text_file_path) as f:
-                    self._text.insert("1.0", f.read())
+                    self._original_text = f.read()
+                    self._text.insert("1.0", self._original_text)
             except IOError:
                 pass
+
+    def destroy(self):
+        if self._commands_text_file_path is not None:
+            text = self._text.get("1.0", tkinter.END)
+            if text.strip() != self._original_text.strip():
+                save_result = messagebox.askyesno(
+                    "Save changes?",
+                    "The commands text has changed.\n"
+                    "Do you want to overwrite the input file?"
+                )
+                if save_result:
+                    with open(self._commands_text_file_path, 'w') as f:
+                        f.write(text)
+                    print("Commands overwritten to", self._commands_text_file_path)
+                else:
+                    print("Changed discarded")
+        tkinter.Tk.destroy(self)
 
 
 def main():
