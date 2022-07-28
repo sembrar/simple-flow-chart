5487c1084ac247cd85ed884f87b2bac3ebe05811
sembrar <sagarpj95@gmail.com>
2022-07-28 09:14:40 +0530

    Scrollregion for canvas is set after running each flow-chart-command
    On branch main
    Changes to be committed:
     modified:   main.py

--- main.py
+++ main.py
@@ -311,6 +311,9 @@ class FlowChart(tkinter.Tk):
             messagebox.showerror("Error occurred", "Please see command prompt window for details")
             raise e
 
+        finally:
+            self._set_canvas_scroll_region()
+
     def _get_x_middle_y_lower_from_lowest_canvas_object(self):
         x = None
         y = 0
@@ -387,6 +390,27 @@ class FlowChart(tkinter.Tk):
         if corner == "east":
             return x2, int((y1 + y2) / 2)
         raise ValueError("corner {} is unknown".format(corner))
+
+    def _set_canvas_scroll_region(self):
+        all_canvas_objects = self._canvas.find_all()
+        if len(all_canvas_objects) == 0:
+            return
+
+        bboxes = tuple(map(lambda x: self._canvas.bbox(x), all_canvas_objects))
+
+        if len(all_canvas_objects) == 1:
+            x_min, y_min, x_max, y_max = bboxes[0]
+        else:
+            x_min = min(bboxes, key=lambda b: b[0])[0]
+            x_max = max(bboxes, key=lambda b: b[2])[2]
+            y_min = min(bboxes, key=lambda b: b[1])[1]
+            y_max = max(bboxes, key=lambda b: b[3])[3]
+
+        scroll_region = (
+            x_min - PADDING_FOR_NEW_OBJECT, y_min - PADDING_FOR_NEW_OBJECT,
+            x_max + PADDING_FOR_NEW_OBJECT, y_max + PADDING_FOR_NEW_OBJECT)
+
+        self._canvas.configure(scrollregion=scroll_region)
 
 
 def main():
