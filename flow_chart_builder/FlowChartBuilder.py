4fd88a6f7c1fa83a6e1fe340ca2f47276e483040
sembrar <sagarpj95@gmail.com>
2022-08-09 20:32:04 +0530

    Created and tested SelectOnlyCombobox that can have fixed choices or automatically updatable choices
    On branch gui-for-creating-flow-chart
    Changes to be committed:
     modified:   flow_chart_builder/FlowChartBuilder.py

--- flow_chart_builder/FlowChartBuilder.py
+++ flow_chart_builder/FlowChartBuilder.py
@@ -34,9 +34,41 @@ class IntEntry(ttk.Entry):
             return False
 
 
+class SelectOnlyCombobox(ttk.Combobox):
+
+    def __init__(self, master=None, func_get_choices=None, **kw):
+        """
+        :param master:
+        :param func_get_choices: a function can be specified that returns a list/tuple of choice
+            this can be used to update choices dynamically, as it will be called whenever the list of choices is
+            pulled down which triggers a button down event
+        :param kw:
+        """
+        super().__init__(master, **kw)
+        self._func_get_choices = func_get_choices
+        self.configure(validate="key", validatecommand=self._restrict_keyboard_input)
+        if self._func_get_choices is not None:
+            self.bind("<Button-1>", self._run_function_to_update_choices)
+
+    def _restrict_keyboard_input(self):
+        return False
+
+    def _run_function_to_update_choices(self, _):
+        self.configure(values=self._func_get_choices())
+
+
 if __name__ == '__main__':
-    # DEBUG = True
+    DEBUG = True
+
+    import time
+
+
+    def get_time():
+        return time.time(), time.ctime()
+
     root = tkinter.Tk()
-    widget = IntEntry(root)
+    widget = SelectOnlyCombobox(root, func_get_choices=get_time)
     widget.grid(row=0, column=0)
+    widget2 = SelectOnlyCombobox(root, values="north east south west".split())
+    widget2.grid(row=1, column=0)
     root.mainloop()
