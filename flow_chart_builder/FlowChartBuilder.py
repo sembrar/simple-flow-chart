57e3a85a08efe22996fadf14169f2f78a6b4490f
sembrar <sagarpj95@gmail.com>
2022-08-10 14:08:39 +0530

    A single command frame is created that shows different settings based on command type along with some helper frames
    On branch gui-for-creating-flow-chart
    Changes to be committed:
     modified:   flow_chart_builder/FlowChartBuilder.py

--- flow_chart_builder/FlowChartBuilder.py
+++ flow_chart_builder/FlowChartBuilder.py
@@ -57,18 +57,106 @@ class SelectOnlyCombobox(ttk.Combobox):
         self.configure(values=self._func_get_choices())
 
 
+AVAILABLE_COMMAND_TYPES = "start stop operation decision connection delete delete-all title box font connector".split()
+
+DETAIL_TYPES_FOR_COMMANDS = dict()
+DETAIL_TYPES_FOR_COMMANDS["start"] = ("name", "placement", "autostart", "dx", "dy")
+DETAIL_TYPES_FOR_COMMANDS["stop"] = DETAIL_TYPES_FOR_COMMANDS["start"]
+DETAIL_TYPES_FOR_COMMANDS["operation"] = ("name", "text", "placement", "autostart", "dx", "dy")
+DETAIL_TYPES_FOR_COMMANDS["decision"] = DETAIL_TYPES_FOR_COMMANDS["operation"]
+DETAIL_TYPES_FOR_COMMANDS["connection"] = ("start", "end", "points", "autostart", "label", "label-dx", "label-dy", "label-color")
+DETAIL_TYPES_FOR_COMMANDS["delete"] = ("name", "autostart")
+DETAIL_TYPES_FOR_COMMANDS["delete-all"] = ("autostart",)
+DETAIL_TYPES_FOR_COMMANDS["title"] = ("text", "autostart")
+DETAIL_TYPES_FOR_COMMANDS["box"] = ("width", "autostart")
+DETAIL_TYPES_FOR_COMMANDS["font"] = ("size", "weight", "autostart")
+DETAIL_TYPES_FOR_COMMANDS["connector"] = ("width", "autostart")
+
+
+class CombosNameAndDirection(ttk.Frame):
+
+    def __init__(self, master=None, func_to_get_names=None, **kw):
+        super().__init__(master, **kw)
+
+        self._func_to_get_names = func_to_get_names
+        self._name_combo = SelectOnlyCombobox(self, self._func_to_get_names)
+        self._name_combo.grid(row=0, column=0)
+
+        self._direction_combo = SelectOnlyCombobox(self, values=("north", "east", "south", "west"))
+        self._direction_combo.grid(row=0, column=1)
+
+
+class Points(ttk.Frame):
+
+    def __init__(self, master=None, **kw):
+        super().__init__(master, **kw)
+
+        ttk.Label(self, text="Placeholder").grid(row=0, column=0)
+
+
+class SingleCommandFrame(ttk.Frame):
+    def __init__(self, master=None, **kw):
+        super().__init__(master, **kw)
+
+        label_frame_type = ttk.Labelframe(self, text="Type")
+        label_frame_type.grid(row=0, column=0, sticky='w')
+
+        self._var_command_type = tkinter.StringVar(self)
+
+        self._type = SelectOnlyCombobox(
+            label_frame_type, textvariable=self._var_command_type, values=AVAILABLE_COMMAND_TYPES)
+        self._type.grid(row=0, column=0)
+
+        self._chosen_settings = {}
+
+        self._var_command_type.trace_variable("w", self._changed_command_type)
+
+    def _changed_command_type(self, *_):
+        available_detail_types = DETAIL_TYPES_FOR_COMMANDS.get(self._var_command_type.get(), None)
+
+        if available_detail_types is None:
+            print("There are no detail types available for", self._var_command_type.get())
+            return
+
+        self._remove_existing_detail_frames()
+
+        for d, i in zip(available_detail_types, range(len(available_detail_types))):
+            label_frame = ttk.Labelframe(self, text=d)
+            label_frame.grid(row=0, column=i+1)  # the column=0 is for "Type" i.e. command type
+
+            if d == "name" or d == "text" or d == "label" or d == "label-color":
+                child = ttk.Entry(label_frame)
+            elif d == "placement" or d == "start" or d == "end":
+                child = CombosNameAndDirection(label_frame)  # todo pass function to get names for combo
+            elif d == "autostart":
+                child = SelectOnlyCombobox(label_frame, values=("False", "True"))
+            elif d == "dx" or d == "dy" or d == "label-dx" or d == "label-dy" or d == "width" or d == "size":
+                child = IntEntry(label_frame)
+            elif d == "points":
+                child = Points(label_frame)
+            elif d == "weight":
+                child = SelectOnlyCombobox(label_frame, values=("normal", "bold"))
+            else:
+                print("Unknown detail type:", d)
+                return
+
+            child.grid(row=0, column=0)
+
+    def _remove_existing_detail_frames(self):
+        children = self.winfo_children()
+        for child in children:
+            detail_name = child.cget("text").lower()
+            if detail_name == "type":
+                continue
+            # todo save the value of the child to the dictionary for use later
+            child.destroy()
+            debug_print("Removed detail:", detail_name)
+
+
 if __name__ == '__main__':
     DEBUG = True
 
-    import time
-
-
-    def get_time():
-        return time.time(), time.ctime()
-
     root = tkinter.Tk()
-    widget = SelectOnlyCombobox(root, func_get_choices=get_time)
+    widget = SingleCommandFrame(root)
     widget.grid(row=0, column=0)
-    widget2 = SelectOnlyCombobox(root, values="north east south west".split())
-    widget2.grid(row=1, column=0)
     root.mainloop()
