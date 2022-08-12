4fd3f7f0220ddf028b27f0e8a1ec22066116b4d4
sembrar <sagarpj95@gmail.com>
2022-08-12 21:06:01 +0530

    Created FlowChartTextFrame class that inherits from ScrolledTextFrame
    On branch refactor-flow-chart-class
    Changes to be committed:
     modified:   flow_chart_builder/FlowChartBuilderText.py

--- flow_chart_builder/FlowChartBuilderText.py
+++ flow_chart_builder/FlowChartBuilderText.py
@@ -1,11 +1,15 @@
-from tkinter import ttk
-
 from scrolled_widgets.ScrolledTextFrame import ScrolledTextFrame
+
+
+class FlowChartTextFrame(ScrolledTextFrame):
+    def __init__(self, master=None, kwargs_for_frame=None, **kw_for_text_widget):
+        super().__init__(master, horizontal_scroll=True, kwargs_for_frame=kwargs_for_frame, **kw_for_text_widget)
 
 
 def main():
     from tkinter import Tk
     root = Tk()
-    widget = ScrolledTextFrame()
+    root.title("FlowChartTextFrame")
+    widget = FlowChartTextFrame()
     widget.grid(row=0, column=0, sticky='news')
     root.mainloop()
