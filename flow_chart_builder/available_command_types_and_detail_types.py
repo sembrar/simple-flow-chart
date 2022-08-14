68da2185c209f8783c6a247c0765a4df3fabdc2e
sembrar <sagarpj95@gmail.com>
2022-08-14 22:52:48 +0530

    Removed "autostart" from available detail names as all commands are run together
    On branch main
    Changes to be committed:
     modified:   flow_chart_builder/available_command_types_and_detail_types.py

--- flow_chart_builder/available_command_types_and_detail_types.py
+++ flow_chart_builder/available_command_types_and_detail_types.py
@@ -1,15 +1,15 @@
 AVAILABLE_COMMAND_TYPES = "start stop operation decision connection delete delete-all title box font connector".split()
 
 DETAIL_TYPES_FOR_COMMANDS = dict()
-DETAIL_TYPES_FOR_COMMANDS["start"] = ("name", "placement", "autostart", "dx", "dy")
+DETAIL_TYPES_FOR_COMMANDS["start"] = ("name", "placement", "dx", "dy")
 DETAIL_TYPES_FOR_COMMANDS["stop"] = DETAIL_TYPES_FOR_COMMANDS["start"]
-DETAIL_TYPES_FOR_COMMANDS["operation"] = ("name", "text", "placement", "autostart", "dx", "dy")
+DETAIL_TYPES_FOR_COMMANDS["operation"] = ("name", "text", "placement", "dx", "dy")
 DETAIL_TYPES_FOR_COMMANDS["decision"] = DETAIL_TYPES_FOR_COMMANDS["operation"]
-DETAIL_TYPES_FOR_COMMANDS["connection"] = ("start", "end", "points", "autostart",
+DETAIL_TYPES_FOR_COMMANDS["connection"] = ("start", "end", "points",
                                            "label", "label-dx", "label-dy", "label-color")
-DETAIL_TYPES_FOR_COMMANDS["delete"] = ("name", "autostart")
-DETAIL_TYPES_FOR_COMMANDS["delete-all"] = ("autostart",)
-DETAIL_TYPES_FOR_COMMANDS["title"] = ("text", "autostart")
-DETAIL_TYPES_FOR_COMMANDS["box"] = ("width", "autostart")
-DETAIL_TYPES_FOR_COMMANDS["font"] = ("size", "weight", "autostart")
-DETAIL_TYPES_FOR_COMMANDS["connector"] = ("width", "autostart")
+DETAIL_TYPES_FOR_COMMANDS["delete"] = ("name",)
+DETAIL_TYPES_FOR_COMMANDS["delete-all"] = ()
+DETAIL_TYPES_FOR_COMMANDS["title"] = ("text",)
+DETAIL_TYPES_FOR_COMMANDS["box"] = ("width",)
+DETAIL_TYPES_FOR_COMMANDS["font"] = ("size", "weight")
+DETAIL_TYPES_FOR_COMMANDS["connector"] = ("width",)
